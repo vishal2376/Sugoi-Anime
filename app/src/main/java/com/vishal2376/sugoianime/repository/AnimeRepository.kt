600260edb90e7e943023b5a72955d1b9b466072e
vishal2376 <vishalsingh2376@gmail.com>
2022-11-07 23:23:36 +0530

    anime repository created

---
+++ app/src/main/java/com/vishal2376/sugoianime/repository/AnimeRepository.kt
@@ -0,0 +1,20 @@
+package com.vishal2376.sugoianime.repository
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.vishal2376.sugoianime.api.AnimeService
+import com.vishal2376.sugoianime.models.AnimeList
+
+class AnimeRepository(private val animeService: AnimeService) {
+
+    private val _popularAnime = MutableLiveData<AnimeList>()
+
+    val popularAnime: LiveData<AnimeList> get() = _popularAnime
+
+    suspend fun getPopularAnime(){
+        val result = animeService.getPopularAnime()
+        if(result.body() != null){
+            _popularAnime.postValue(result.body())
+        }
+    }
+}

1df658bea5f3d140c77c74af90088c87332c3b06
vishal2376 <vishalsingh2376@gmail.com>
2022-11-07 23:08:53 +0530

    models created

---
+++ app/src/main/java/com/vishal2376/sugoianime/models/AnimeListItem.kt
@@ -0,0 +1,9 @@
+package com.vishal2376.sugoianime.models
+
+data class AnimeListItem(
+    val animeId: String,
+    val animeImg: String,
+    val animeTitle: String,
+    val animeUrl: String,
+    val releasedDate: String
+)

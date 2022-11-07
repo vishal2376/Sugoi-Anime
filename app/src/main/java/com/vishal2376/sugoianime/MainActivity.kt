76b65406c3934b951c79afd74efbc269f7e7bfa8
vishal2376 <vishalsingh2376@gmail.com>
2022-11-08 00:17:53 +0530

    testing method added

--- app/src/main/java/com/vishal2376/sugoianime/MainActivity.kt
+++ app/src/main/java/com/vishal2376/sugoianime/MainActivity.kt
@@ -2,10 +2,23 @@ package com.vishal2376.sugoianime
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.lifecycle.ViewModelProvider
+import com.vishal2376.sugoianime.viewmodels.AnimeViewModalFactory
+import com.vishal2376.sugoianime.viewmodels.AnimeViewModel
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //testing
+        val repository = (application as AnimeApplication).repository
+        val viewModel = ViewModelProvider(this,AnimeViewModalFactory(repository))[AnimeViewModel::class.java]
+
+        viewModel.popularAnime.observe(this){
+            Log.e("MainActivity",it.toString())
+        }
+
     }
 }

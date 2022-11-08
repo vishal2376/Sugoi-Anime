904e225ce943e95a449da1013d69269bfa9760ec
vishal2376 <vishalsingh2376@gmail.com>
2022-11-08 23:16:12 +0530

    testing code added

--- app/src/main/java/com/vishal2376/sugoianime/fragments/nav/MovieFragment.kt
+++ app/src/main/java/com/vishal2376/sugoianime/fragments/nav/MovieFragment.kt
@@ -1,13 +1,26 @@
 package com.vishal2376.sugoianime.fragments.nav
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.vishal2376.sugoianime.AnimeApplication
 import com.vishal2376.sugoianime.R
+import com.vishal2376.sugoianime.adapters.MovieAdapter
+import com.vishal2376.sugoianime.repository.AnimeRepository
+import com.vishal2376.sugoianime.viewmodels.AnimeViewModalFactory
+import com.vishal2376.sugoianime.viewmodels.AnimeViewModel
 
 class MovieFragment : Fragment() {
+
+    private lateinit var repository: AnimeRepository
+    private lateinit var viewModel: AnimeViewModel
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -17,4 +30,26 @@ class MovieFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_movies, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        //--------------------------testing-------------------------------------
+        // TODO : remove this after data binding
+
+        repository = (requireActivity().application as AnimeApplication).repository
+        viewModel =
+            ViewModelProvider(this, AnimeViewModalFactory(repository))[AnimeViewModel::class.java]
+
+        val recyclerView = requireActivity().findViewById<RecyclerView>(R.id.rvMovies)
+
+        recyclerView.layoutManager = LinearLayoutManager(requireContext())
+
+        viewModel.movieAnime.observe(requireActivity()) {
+            Log.e("@@@",it.toString())
+            recyclerView.adapter = MovieAdapter(requireContext(), it)
+        }
+
+        //------------------------------------------------------------------------------
+
+    }
 }

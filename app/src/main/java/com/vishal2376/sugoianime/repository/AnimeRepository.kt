4afc05d399cf56d70796ae5a45bd48016543893a
vishal2376 <vishalsingh2376@gmail.com>
2022-11-09 22:26:46 +0530

    define getAnimeDetail() func

--- app/src/main/java/com/vishal2376/sugoianime/repository/AnimeRepository.kt
+++ app/src/main/java/com/vishal2376/sugoianime/repository/AnimeRepository.kt
@@ -4,14 +4,17 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.vishal2376.sugoianime.api.AnimeService
 import com.vishal2376.sugoianime.models.AnimeList
+import com.vishal2376.sugoianime.models.detail.AnimeDetail
 
 class AnimeRepository(private val animeService: AnimeService) {
 
     private val _popularAnime = MutableLiveData<AnimeList>()
     private val _movieAnime = MutableLiveData<AnimeList>()
+    private val _animeDetail = MutableLiveData<AnimeDetail>()
 
     val popularAnime: LiveData<AnimeList> get() = _popularAnime
     val movieAnime: LiveData<AnimeList> get() = _movieAnime
+    val animeDetail: LiveData<AnimeDetail> get() = _animeDetail
 
     suspend fun getPopularAnime(){
         val result = animeService.getPopularAnime()
@@ -20,10 +23,17 @@ class AnimeRepository(private val animeService: AnimeService) {
         }
     }
 
-    suspend fun getMovieAnime(pageNumber:Int){
+    suspend fun getMovieAnime(pageNumber: Int) {
         val result = animeService.getMovieAnime(pageNumber)
-        if(result.body() != null){
+        if (result.body() != null) {
             _movieAnime.postValue(result.body())
+        }
+    }
+
+    suspend fun getAnimeDetail(animeID: String) {
+        val result = animeService.getAnimeDetail(animeID)
+        if (result.body() != null) {
+            _animeDetail.postValue((result.body()))
         }
     }
 }

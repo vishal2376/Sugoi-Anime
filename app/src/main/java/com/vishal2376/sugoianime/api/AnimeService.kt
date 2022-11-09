5cbb82b62df85e1c4fcf7ba9dbb21137efdbd9c6
vishal2376 <vishalsingh2376@gmail.com>
2022-11-09 22:23:53 +0530

    add anime detail api

--- app/src/main/java/com/vishal2376/sugoianime/api/AnimeService.kt
+++ app/src/main/java/com/vishal2376/sugoianime/api/AnimeService.kt
@@ -1,18 +1,24 @@
 package com.vishal2376.sugoianime.api
 
 import com.vishal2376.sugoianime.models.AnimeList
+import com.vishal2376.sugoianime.models.detail.AnimeDetail
 import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface AnimeService {
 
     @GET("/popular")
-    suspend fun getPopularAnime() :Response<AnimeList>
+    suspend fun getPopularAnime(): Response<AnimeList>
 
     @GET("/anime-movies")
     suspend fun getMovieAnime(
         @Query("page") page: Int
     ): Response<AnimeList>
 
+    @GET("/anime-details/{animeID}")
+    suspend fun getAnimeDetail(
+        @Path("animeID") animeID: String
+    ): Response<AnimeDetail>
 }

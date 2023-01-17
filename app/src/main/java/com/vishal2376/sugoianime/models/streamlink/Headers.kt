099e66fc00a198c1fd8926db44d314dc5643dc08
Vishal Singh <vishalsingh2376@gmail.com>
2023-01-17 16:13:49 +0530

    fix typo

--- app/src/main/java/com/vishal2376/sugoianime/models/streamlink/Headers.kt
+++ app/src/main/java/com/vishal2376/sugoianime/models/streamlink/Headers.kt
@@ -2,6 +2,6 @@ package com.vishal2376.sugoianime.models.streamlink
 
 data class Headers(
     val Referer: String,
-    val User-Agent: String,
+    val UserAgent: String,
     val watchsb: String
 )
